6ccf52c2e636c2ad63030e2da9fa7c0ed43e18a3
Joel Thompson <chameleonsrock@live.com>
2015-01-20 15:29:57 -0500

    Refactored the rate retriever into a seperate function and tested both normal and inverse exchange rates.

--- currency.py
+++ currency.py
@@ -1,15 +1,26 @@
-rates = [('USD', 'EUR', 0.86),
-         ('EUR', 'USD', 1.15)]
+rates = [('USD', 'EUR', 0.86)]
 
 
-def convert(rate, value, start, to):
+def get_rate(start, to):
+    """Returns the exchange rate based on the starting currency to the ending
+    currency."""
+    for tup in rates:
+        if start == tup[0] and to == tup[1]:
+            rate = tup[2]
+            return rate
+        elif start == tup[1] and to == tup[0]:
+            rate = round(1/tup[2], 2)
+            return rate
+
+
+
+def convert(rates, value, start, to):
     """Takes a rate from a list of exchange rates and applies it to a value by
     by converting from the "start" variable to the "to" variable by the
     corresponding rate."""
-    rate = [tuples[2]
-             for tuples in rates
-             if start == tuples[0] and to == tuples[1]]
+    rate = get_rate(start, to)
     if start == to:
         return value
     else:
-        return value * rate[0]
+        rate = get_rate(start, to)
+        return value * rate
